5ad3048cdb3877d2e60e2fc32f63848ab1e92a8a
tayuprosper <prospertayu2@gmail.com>
2026-02-16 19:41:26 +0100

    initial commit

---
+++ backend/src/Components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+
+    const navigationItems = [
+        {id: 1, name: ''}
+    ]
+
+  return (
+    <div className='text-3xl font-bold'>
+        
+    </div>
+  )
+}
+
+export default Navbar;
