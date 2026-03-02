608803165a50ba12d12ce3c43a8c8ba7597eb136
tayuprosper <prospertayu2@gmail.com>
2026-03-03 00:10:55 +0100

    Remove ghost folder that only existed on GitHub

--- backend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import './App.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './Components/router'
-function App() {
-
-  return <RouterProvider router={router}/>
-}
-
-export default App
